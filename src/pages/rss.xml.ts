745b8a5ca3d649a693e1dd77c951b05d115b6775
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2025-04-11 21:18:22 +0200

    Mejora  de RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import type { APIRoute } from "astro";
 import rss from '@astrojs/rss';
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
     const blogPosts = await getCollection('blog');
@@ -10,6 +14,11 @@ export const GET: APIRoute = async ({ params, request, site }) => {
         title: 'Blog de Pablo',
         // `<description>` campo en el xml generado
         description: 'Un simple blog de Pablo',
+
+        xmlns: {
+            media: 'http://search.yahoo.com/mrss/',
+          },
+          
         // Usa el "site" desde el contexto del endpoint
         // https://docs.astro.build/en/reference/api-reference/#contextsite
         site: site ?? '',
@@ -20,6 +29,17 @@ export const GET: APIRoute = async ({ params, request, site }) => {
             description: post.data.description,
             pubDate: post.data.date,
             link: `/posts/${post.slug}`,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+              }),
+              
+              customData: `<media:content
+                  type="image/${post.data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                  width="${post.data.image.width}"
+                  height="${post.data.image.height}"
+                  medium="image"
+                  url="${site + post.data.image.src}" />
+              `,
         })),
         // (opcional) inyecta xml personalizado
         customData: `<language>es-es</language>`,
